d0e9bd85ecede4c8301ef90e403d1be04a0e721b
Sujata More <sujatamore660@gmail.com>
2023-01-27 01:05:56 +0530

    add demo file

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int c=45;
+int main(){
+	int a,b;
+    int c;
+	cout<<"enter the value of integer a"<<endl;
+	cin>>a;
+	cout<<"enter the value of integer b"<<endl;
+	cin>>b;
+	cout<<"the value of global c is"<<endl<<:: c;
+	return 0;
+	
+}
